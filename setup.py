aedf3f28dbd2a030772c88fc93c3ad0430f4d3b6
Rob McQueen <robmcqn@gmail.com>
2015-11-09 10:15:21 -0800

    initial commit. basic scraper works

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tpp2json',
+    version='1.0',
+    description='Making the TPP machine readable',
+    author='systemizer',
+    author_email='dj@systemizer.me',
+    url='djsystemizer.com',
+    scripts=["bin/scrape-tpp"]
+)
